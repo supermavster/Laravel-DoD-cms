c3c06fd620dc9108fc827e8b74c3d5021aa92ca5
Mavsters <miguel@appsolution.co>
2019-07-15 08:37:01 -0500

    [Add]  Paypal

--- config/paypal.php
+++ config/paypal.php
@@ -1,12 +1,42 @@
 <?php
 return [
-    'client_id' => env('PAYPAL_CLIENT_ID', ''),
-    'secret' => env('PAYPAL_SECRET', ''),
-    'settings' => array(
-        'mode' => env('PAYPAL_MODE', 'sandbox'),
+    'client_id' => env('PAYPAL_CLIENT_ID'),
+    'secret' => env('PAYPAL_CLIENT_SECRET'),
+
+    /**
+     * SDK configuration
+     */
+    'settings' => [
+
+        /**
+         * Available option 'sandbox' or 'live'
+         */
+        'mode' => 'sandbox',
+
+        /**
+         * Specify the max request time in seconds
+         */
         'http.ConnectionTimeOut' => 30,
+
+        /**
+         * Whether want to log to a file
+         */
         'log.LogEnabled' => true,
+
+        /**
+         * Specify the file that want to write on
+         */
         'log.FileName' => storage_path() . '/logs/paypal.log',
-        'log.LogLevel' => 'ERROR'
-    ),
+
+        /**
+         * Available option 'FINE', 'INFO', 'WARN' or 'ERROR'
+         *
+         * Logging is most verbose in the 'FINE' level and decreases as you
+         * proceed towards ERROR
+         */
+        'log.LogLevel' => 'FINE'
+    ],
+    'webhooks' => [
+        'payment_sale_completed' => env('PAYPAL_PAYMENT_SALE_COMPLETED_WEBHOOK_ID'),
+    ],
 ];
