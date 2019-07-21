165c2dce1fba1e23bbbdda8ccab770dde35d1764
Mavsters <miguel@appsolution.co>
2019-07-21 11:44:54 -0500

    [Add] Base auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,23 +12,23 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/55799d9ca4.js"></script>
 
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-@auth
-    @include('layouts.navbar')
-@endauth
-<div class="container col-md-11 mx-auto my-5">
-    @yield('content')
-</div>
 
-<!-- Scripts -->
-<script src="{{ asset('js/app.js') }}"></script>
+<body>
+    @auth
+    @include('layouts.navbar')
+    @endauth
+    <div class="container col-md-11 mx-auto my-5">
+        @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
-p
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Auth::routes();
 Route::get('/', function () {
     return view('welcome');
-})->middleware('auth');
+});
 
 Route::get('verify/{code}', 'Api\UserController@verify');
 
